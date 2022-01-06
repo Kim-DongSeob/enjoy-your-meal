609d9609d2f96a53101e74740f66d30a4dee4284
Kim-DongSeob <imjambo0916@gmail.com>
2022-01-06 15:29:22 +0900

    Fix: script.js

--- static/js/script.js
+++ static/js/script.js
@@ -195,7 +195,7 @@ function search() {
                                             
                                         <div class="like-btn">
                                             <div class="like-count">${likeCount}</div>
-                                            <div onclick="handleClickLike('${title}')">
+                                            <div onclick="handleClickLike('${title}')" class="click">
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#e91550" class="bi bi-heart-fill" viewBox="0 0 16 16">
                                                   <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
                                                 </svg>
