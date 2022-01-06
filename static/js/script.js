724dcc6a761ddc56bc000b0776684d3b2ff4ec16
Kim-DongSeob <imjambo0916@gmail.com>
2022-01-07 04:40:38 +0900

    Fix: 스타일 변경

--- static/js/script.js
+++ static/js/script.js
@@ -202,9 +202,7 @@ function search() {
                                         <div class="like-btn">
                                             <div class="like-count">${likeCount}</div>
                                             <div onclick="handleClickLike('${title}')" class="click">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#e91550" class="bi bi-heart-fill" viewBox="0 0 16 16">
-                                                  <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
-                                                </svg>
+                                                <img src="/static/img/icon/suit-heart-fill.svg"/>
                                             </div>
                                         </div>
                                     </div>
@@ -220,9 +218,7 @@ function search() {
                                         <div class="like-btn">
                                             <div class="like-count">${likeCount}</div>
                                             <div onclick="handleClickLike('${title}')" class="click">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#e91550" class="bi bi-heart" viewBox="0 0 16 16">
-                                                  <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
-                                                </svg>
+                                                <img src="/static/img/icon/suit-heart.svg"/>
                                             </div>
                                         </div>
                                     </div>
@@ -322,9 +318,7 @@ function sortLikeList() {
                                         <div class="like-btn">
                                             <div class="like-count">${likeCount}</div>
                                             <div onclick="handleClickLike('${name}')" class="click">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#e91550" class="bi bi-heart-fill" viewBox="0 0 16 16">
-                                                  <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
-                                                </svg>
+                                                <img src="/static/img/icon/suit-heart-fill.svg"/>
                                             </div>
                                         </div>
                                     </div>
@@ -339,9 +333,7 @@ function sortLikeList() {
                                         <div class="like-btn">
                                             <div class="like-count">${likeCount}</div>
                                             <div onclick="handleClickLike('${name}')" class="click">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#e91550" class="bi bi-heart" viewBox="0 0 16 16">
-                                                  <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
-                                                </svg>
+                                                <img src="/static/img/icon/suit-heart.svg"/>
                                             </div>
                                         </div>
                                     </div>
@@ -385,9 +377,7 @@ function randomRecommend() {
                                         <div class="like-btn">
                                             <div class="like-count">${likeCount}</div>
                                             <div onclick="handleClickLike('${name}')" class="click">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#e91550" class="bi bi-heart-fill" viewBox="0 0 16 16">
-                                                  <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
-                                                </svg>
+                                                <img src="/static/img/icon/suit-heart-fill.svg"/>
                                             </div>
                                         </div>
                                     </div>
@@ -402,9 +392,7 @@ function randomRecommend() {
                                         <div class="like-btn">
                                             <div class="like-count">${likeCount}</div>
                                             <div onclick="handleClickLike('${name}')" class="click">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#e91550" class="bi bi-heart" viewBox="0 0 16 16">
-                                                  <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
-                                                </svg>
+                                                <img src="/static/img/icon/suit-heart.svg"/>
                                             </div>
                                         </div>
                                         </div>
